ad9dd44413af4f904db5131a0bd7c1185dcd1d6f
adamzeng <khklara3@gmail.com>
2019-06-20 14:48:14 +0800

    bug-fixedC

--- echo_selectserv.c
+++ echo_selectserv.c
@@ -11,41 +11,56 @@
 #include <sys/time.h>
 #include <sys/select.h>
 
-#define BUF_SIZE 32
+#define BUFF_SIZE 32
+#define TRUE 1
+#define FALSE 1
 
 void error_handling(char *message);
 
 int main(int argc, char **argv) {
-    int serv_sock, clnt_sock;
-    struct sockaddr_in serv_adr, clnt_adr;
+    int server_sock, client_sock, option;
+    struct sockaddr_in server_addr, client_addr;
     struct timeval timeout, timeout_init;
     fd_set reads, reads_init;
 
-    socklen_t adr_sz;
+    socklen_t client_addr_size, optlen;
     int fd_max, str_len, fd_num, i;
-    char buf[BUF_SIZE];
-    if (argc != 2) {
+    char buff[BUFF_SIZE];
+    if (argc != 2) { //命令行中启动服务程序仅限一个参数：端口号
         printf("Usage : %s <port>\n", argv[0]);
         exit(1);
     }
 
-    serv_sock = socket(PF_INET, SOCK_STREAM, 0);
-    memset(&serv_adr, 0, sizeof(serv_adr));
-    serv_adr.sin_family = AF_INET;
-    serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_adr.sin_port = htons(atoi(argv[1]));
+    //调用socket函数创建套接字
+    server_sock = socket(PF_INET, SOCK_STREAM, 0);
+    if (-1 == server_sock) {
+        error_handling("socket() error.");
+    }
 
-    if (bind(serv_sock, (struct sockaddr *) &serv_adr, sizeof(serv_adr)) == -1) {
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(atoi(argv[1]));
+
+    optlen = sizeof(option);
+    option = TRUE;
+    setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, (void *) &option, optlen);
+
+    //调用bind函数分配IP地址和端口号
+    if (-1 == bind(server_sock, (struct sockaddr *) &server_addr,
+                   sizeof(server_addr))) {
         error_handling("bind() error");
     }
-    if (listen(serv_sock, 5) == -1) {
+
+    //监听端口的连接请求,连接请求等待队列size为5
+    if (-1 == listen(server_sock, 5)) {
         error_handling("listen() error");
     }
 
     FD_ZERO(&reads_init);
-    FD_SET(serv_sock, &reads_init);
+    FD_SET(server_sock, &reads_init);
     FD_SET(0, &reads_init);// stdin also works
-    fd_max = serv_sock;
+    fd_max = server_sock;
 
     timeout_init.tv_sec = 5;
     timeout_init.tv_usec = 0;
@@ -53,47 +68,54 @@ int main(int argc, char **argv) {
     while (1) {
         reads = reads_init; // recover initial fd_set reads
         timeout = timeout_init;
-        if ((fd_num = select(fd_max + 1, &reads_init, 0, 0, &timeout)) == -1) {
+        if ((fd_num = select(fd_max + 1, &reads, 0, 0, &timeout)) == -1) {
+            fputs("Error select()!", stderr);
             break; // error occur
         }
         if (fd_num == 0) { // overtime
+            puts("Time-out!");
             continue;
         }
-        for (int i = 0; i < fd_max + 1; ++i) { // iterate every bits in fd_set, if it's set to a file descriptor before
-            if (FD_ISSET(i, &reads)) {     // judge if it's serv_sock or clnt_sock
-                if (i == serv_sock) { // serv_sock change, new client requires to connect
-                    adr_sz = sizeof(clnt_adr);
-                    clnt_sock = accept(serv_sock, (struct sockaddr *) &clnt_adr, &adr_sz);
-                    if (clnt_sock == -1) {
+        for (i = 0; i <= fd_max; i++) {
+            if (FD_ISSET(i, &reads)) {
+                if (i == server_sock) {//connection request!
+                    //接受连接请求
+                    client_addr_size = sizeof(client_addr);
+                    client_sock = accept(server_sock, (struct sockaddr *) &client_addr, &client_addr_size);
+                    //accept函数自动创建数据I/0 socket
+                    if (-1 == client_sock) {
                         error_handling("accept() error");
+                        //健壮性不佳，程序崩溃退出
+                    } else {
+                        //注册与客户端连接的套接字文件描述符
+                        FD_SET(client_sock, &reads_init);
+                        if (fd_max < client_sock) fd_max = client_sock;
+                        printf("Connected client : %d\n", client_sock);
                     }
-                    FD_SET(clnt_sock, &reads_init); // register new clnt_sock to fd_set reads
-                    if (fd_max < clnt_sock) {
-                        fd_max = clnt_sock;
+                } else {//read message!
+                    str_len = read(i, buff, BUFF_SIZE);
+                    if (str_len) {//echo to client
+                        buff[str_len] = 0;
+                        printf("Message from client %d: %s", i, buff);
+                        write(i, buff, str_len);
+                    } else { //close connection
+                        FD_CLR(i, &reads_init);
+                        close(i);
+                        printf("Disconnected client %d!\n", i);
                     }
-                    printf("connected client: %d \n", clnt_sock);
-                }
-            } else { // clnt_sock event, read message from clnt_sock
-                str_len = read(i, buf, BUF_SIZE);
-                if (str_len) { // echo to client
-                    buf[str_len] = 0;
-                    printf("Message from client %d: %s", i, buf);
-                    write(i, buf, str_len);
-                } else {
-                    FD_CLR(i, &reads_init); // clear all fd_set reads
-                    close(i);
-                    printf("close client: %d \n", i);
-                }
-            }
-        }
-    }
-    close(serv_sock);
+                }//end of i==|!=server_sock
+            }//end of if(FD_ISSET)
+        }//end of while
+
+    }//end of for
+
+    //断开连接，关闭套接字
+    close(server_sock);
     return 0;
 }
-
 
 void error_handling(char *message) {
     fputs(message, stderr);
     fputc('\n', stderr);
-    exit(1);
+    exit(EXIT_FAILURE);
 }
